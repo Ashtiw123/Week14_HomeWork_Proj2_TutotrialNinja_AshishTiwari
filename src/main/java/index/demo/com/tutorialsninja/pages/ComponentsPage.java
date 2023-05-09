1e11e7492b48e4eff31130dedd2152db442d46ba
Ashtiw123 <ashtiwh007@gmail.com>
2023-05-09 21:02:40 +0100

    My Homework Week 14 Proj 2 tutorials Ninja Ashish Tiwari

---
+++ src/main/java/index/demo/com/tutorialsninja/pages/ComponentsPage.java
@@ -0,0 +1,54 @@
+package index.demo.com.tutorialsninja.pages;
+
+import com.aventstack.extentreports.Status;
+import index.demo.com.tutorialsninja.customlisteners.CustomListeners;
+import index.demo.com.tutorialsninja.utility.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
+
+public class ComponentsPage extends Utility {
+    @CacheLookup
+    @FindBy(xpath = "//body/div[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]")
+    WebElement components;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[contains(text(),'Show AllComponents')]")
+    WebElement showAllComponents;
+
+    public void mouseHoverOnComponentsAndClick(){
+        CustomListeners.test.log(Status.PASS,"mouseHoverOnComponentsAndClick " + components);
+        Reporter.log("mouseHoverOnComponentsAndClick " + components.toString());
+        clickOnElement(components);
+
+    }
+    public void clickOnShowAllComponents(){
+        CustomListeners.test.log(Status.PASS,"clickOnShowAllComponents " + showAllComponents);
+        Reporter.log("clickOnShowAllComponents " + showAllComponents.toString());
+        clickOnElement(showAllComponents);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
